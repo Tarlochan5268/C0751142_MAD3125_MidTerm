14293469604db15a9423f63aed96cfbb0a8681e5
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-08 19:21:43 -0400

    Made Link Class

--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/SpaceXFlight.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/SpaceXFlight.java
@@ -2,5 +2,13 @@ package com.tarlochan.c0751142_mad3125_midterm;
 
 public class SpaceXFlight
 {
-
+    String flight_number;
+    String mission_name;
+    String upcoming;
+    String launch_year;
+    String launch_window;
+    String details;
+    Rocket rocket;
+    LaunchSite launchSite;
+    Links links;
 }
