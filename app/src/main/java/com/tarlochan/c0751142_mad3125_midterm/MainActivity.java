3aaab1e9d8b96a84b70cef02caaa01905f49ee6c
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-08 19:58:34 -0400

    Completed Data Store with 3 Json Methods

--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
@@ -2,6 +2,11 @@ package com.tarlochan.c0751142_mad3125_midterm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("User.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            int count = is.read(buffer);
+            is.close();
+            Log.d("-- COUNT --", String.format("%d Bytes",count));
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
