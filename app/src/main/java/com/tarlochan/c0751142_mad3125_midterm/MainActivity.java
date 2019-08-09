37511b1cb3c27038c1e1fdfd5b58bba02c3349af
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-09 02:16:15 -0400

    Added Activity Main

--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
@@ -2,34 +2,55 @@ package com.tarlochan.c0751142_mad3125_midterm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
 {
+    private RecyclerView recyclerView;
+    private FlightAdapter mAdapter;
     ImageView imageView;
+    DataStore mDataStore;
+    private List<FlightRow> flightRowList = new ArrayList<>();
+    public static ArrayList<SpaceXFlight> staticSpaceXFlightList;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        mDataStore = new DataStore(MainActivity.this);
+        mDataStore.processJSON();
+        MainActivity.staticSpaceXFlightList = mDataStore.mSpaceXFlightList;
+        Log.d("Size of SS space List :",String.valueOf(MainActivity.staticSpaceXFlightList.size()));
+        Log.d("Size of mSpaceList:",String.valueOf(mDataStore.mSpaceXFlightList.size()));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
+        recyclerView = findViewById(R.id.recycleView);
 
 
-        DataStore mDataStore = new DataStore(MainActivity.this);
-        mDataStore.processJSON();
-        Log.d("Size of List : ",String.valueOf(mDataStore.mSpaceXFlightList.size()));
+        mAdapter = new FlightAdapter(this,flightRowList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        prepareFlightListData();
 
-        String photoUrl = mDataStore.mSpaceXFlightList.get(1).getLinks().getMission_patch_small();
-        Glide.with(imageView)  //2
-                .load(photoUrl) //3
-                .centerCrop() //4
-                .placeholder(R.drawable.img_placeholder) //5
-                .error(R.drawable.img_notload) //6
-                .fallback(R.drawable.img_placeholder) //7
-                .into(imageView);
+    }
 
+    private void prepareFlightListData()
+    {
+        for(SpaceXFlight flight : mDataStore.mSpaceXFlightList)
+        {
+            FlightRow flightRow = new FlightRow(flight.getLinks().getMission_patch_small(),flight.getMission_name(),flight.getLaunch_year());
+            flightRowList.add(flightRow);
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
