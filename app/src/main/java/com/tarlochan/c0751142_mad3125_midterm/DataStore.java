2542e8d69c6d3a55f0fbc434e525d50dd47bc1db
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-08 22:52:08 -0400

    added Gradient background

--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/DataStore.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/DataStore.java
@@ -1,7 +1,6 @@
 package com.tarlochan.c0751142_mad3125_midterm;
 
 import android.content.Context;
-import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/MainActivity.java
@@ -3,21 +3,33 @@ package com.tarlochan.c0751142_mad3125_midterm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+
         DataStore mDataStore = new DataStore(MainActivity.this);
         mDataStore.processJSON();
         Log.d("Size of List : ",String.valueOf(mDataStore.mSpaceXFlightList.size()));
+
+        String photoUrl = mDataStore.mSpaceXFlightList.get(1).getLinks().getMission_patch_small();
+        Glide.with(imageView)  //2
+                .load(photoUrl) //3
+                .centerCrop() //4
+                .placeholder(R.drawable.ImagePlaceHolder) //5
+                .error(R.drawable.imageNotLoad) //6
+                .fallback(R.drawable.ImagePlaceHolder) //7
+                .into(imageView);
 
     }
 }
