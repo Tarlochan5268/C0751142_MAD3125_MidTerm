befa84212e8f3af830d5034bea86094cce3bc4a1
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-09 02:15:12 -0400

    Added Flight Row

---
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/FlightRow.java
@@ -0,0 +1,49 @@
+package com.tarlochan.c0751142_mad3125_midterm;
+
+public class FlightRow
+{
+    String flightimage;
+    String flightName;
+    String flightYear;
+
+    public FlightRow() {
+    }
+
+    public FlightRow(String image, String flightName, String flightYear) {
+        this.flightimage = image;
+        this.flightName = flightName;
+        this.flightYear = flightYear;
+    }
+
+    public String getFlightImage() {
+        return flightimage;
+    }
+
+    public void setFlightImage(String image) {
+        this.flightimage = image;
+    }
+
+    public String getFlightName() {
+        return flightName;
+    }
+
+    public void setFlightName(String flightName) {
+        this.flightName = flightName;
+    }
+
+    public String getFlightYear() {
+        return flightYear;
+    }
+
+    public void setFlightYear(String flightYear) {
+        this.flightYear = flightYear;
+    }
+    @Override
+    public String toString() {
+        return "FlightRow{" +
+                "image='" + flightimage + '\'' +
+                ", flightName='" + flightName + '\'' +
+                ", flightYear='" + flightYear + '\'' +
+                '}';
+    }
+}
