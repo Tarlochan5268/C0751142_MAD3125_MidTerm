717e17d35c9fff2820acb57e3675076893180bf7
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-09 17:17:16 -0400

    modified Flight Detail Activity and its java file

--- app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/FlightDetailsActivity.java
+++ app/src/main/java/com/tarlochan/c0751142_mad3125_midterm/FlightDetailsActivity.java
@@ -1,18 +1,61 @@
 package com.tarlochan.c0751142_mad3125_midterm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.tarlochan.c0751142_mad3125_midterm.SpaceXFlight.SpaceXFlight;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FlightDetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.imgWikipedia)
+    ImageView imgWikipedia;
+    @BindView(R.id.imgArticle)
+    ImageView imgArticle;
+    @BindView(R.id.txtName)
+    TextView txtName;
+    @BindView(R.id.txtYear)
+    TextView txtYear;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.imgYoutube)
+    ImageView imgYoutube;
+    @BindView(R.id.txtDetails)
+    TextView txtDetails;
+    @BindView(R.id.txtRocketId)
+    TextView txtRocketId;
+    @BindView(R.id.txtRocketType)
+    TextView txtRocketType;
+    @BindView(R.id.constraintLayout)
+    ConstraintLayout constraintLayout;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.txtRocketName)
+    TextView txtRocketName;
+    @BindView(R.id.txtUpcoming)
+    TextView txtUpcoming;
+    @BindView(R.id.txtSiteId)
+    TextView txtSiteId;
+    @BindView(R.id.txtSiteName)
+    TextView txtSiteName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_details);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +68,43 @@ public class FlightDetailsActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        int position = getIntent().getIntExtra("position", 0);
+        final SpaceXFlight flight = MainActivity.staticSpaceXFlightList.get(position);
+        txtName.setText(flight.getMission_name());
+        txtYear.setText(flight.getLaunch_year());
+        String photoUrl = flight.getLinks().getMission_patch_small();
+        Log.d("patch Link ------->>>", flight.getLinks().getMission_patch_small());
+        Glide.with(imageView)  //2
+                .load(photoUrl) //3
+                .centerCrop() //4
+                .placeholder(R.drawable.img_placeholder) //5
+                .error(R.drawable.img_notload) //6
+                .fallback(R.drawable.img_placeholder) //7
+                .into(imageView);
+        txtDetails.setText(flight.getDetails());
+        String upcoming = "";
+        if (flight.getUpcoming().equals("false")) {
+            upcoming = "No";
+
+        } else {
+            upcoming = "Yes";
+        }
+        txtUpcoming.setText(upcoming);
+        txtRocketId.setText(flight.getRocket().getRocket_id());
+        txtRocketName.setText(flight.getRocket().getRocket_name());
+        txtRocketType.setText(flight.getRocket().getRocket_type());
+        txtSiteId.setText(flight.getLaunchSite().getSite_id());
+        txtSiteName.setText(flight.getLaunchSite().getSite_name());
+
+        imgArticle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlightDetailsActivity.this,ArticleActivity.class);
+                intent.putExtra("link",flight.getLinks().getArticle_link());
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
